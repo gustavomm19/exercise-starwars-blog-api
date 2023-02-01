3cb04d45d78f443ad7116c65884d18e022422be3
gustavomm19 <gustavomm199@gmail.com>
2023-02-01 01:23:17 +0000

    done

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Person, Planet, FavoritePlanet, FavoritePerson
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,128 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_hello():
 
+    persons = db.session.execute(db.select(User).order_by(User.id))
+    person_list = [item for t in persons.all() for item in t]
+
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        'result': [i.serialize() for i in person_list]
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def get_people():
+
+    persons = db.session.execute(db.select(Person).order_by(Person.created))
+    person_list = [item for t in persons.all() for item in t]
+
+    response_body = {
+        'result': [i.serialize() for i in person_list]
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_person(id):
+
+    item = db.session.execute(db.select(Person).filter_by(id=id))
+
+    response_body = {
+        'result': item.first()[0].serialize_single()
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+
+    planets = db.session.execute(db.select(Planet).order_by(Planet.created))
+    planets_list = [item for t in planets.all() for item in t]
+
+    response_body = {
+        'result': [i.serialize() for i in planets_list]
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_planet(id):
+
+    item = db.session.execute(db.select(Planet).filter_by(id=id))
+
+    response_body = {
+        'result': item.first()[0].serialize_single()
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/users/favorites', methods=['GET'])
+def get_favorites():
+    user_id = request.headers.get('user')
+
+    planets = db.session.execute(db.select(FavoritePlanet).filter_by(user_id=user_id).order_by(FavoritePlanet.id))
+    planets_list = [item for t in planets.all() for item in t]
+
+    people = db.session.execute(db.select(FavoritePerson).filter_by(user_id=user_id).order_by(FavoritePerson.id))
+    people_list = [item for t in people.all() for item in t]
+
+    response_body = {
+        'result': {
+            'planets': [i.serialize() for i in planets_list],
+            'people': [i.serialize() for i in people_list],
+        }
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/favorite/planets/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    user_id = request.headers.get('user')
+
+    favoritePlanet = FavoritePlanet(user_id=user_id, planet_id=planet_id)
+    db.session.add(favoritePlanet)
+    db.session.commit()
+
+    response_body = {
+        'result': 'Succesfully added'
+    }
+
+    return jsonify(response_body), 201
+
+@app.route('/favorite/people/<int:person_id>', methods=['POST'])
+def add_favorite_person(person_id):
+    user_id = request.headers.get('user')
+
+    favoritePerson = FavoritePerson(user_id=user_id, person_id=person_id)
+    db.session.add(favoritePerson)
+    db.session.commit()
+
+    response_body = {
+        'result': 'Succesfully added'
+    }
+
+    return jsonify(response_body), 201
+
+@app.route('/favorite/people/<int:favorite_person_id>', methods=['DELETE'])
+def delete_favorite_person(favorite_person_id):
+
+    item = db.session.execute(db.select(FavoritePerson).filter_by(id=favorite_person_id)).first()[0]
+    db.session.delete(item)
+    db.session.commit()
+
+    return jsonify([]), 204
+
+@app.route('/favorite/planets/<int:favorite_planet_id>', methods=['DELETE'])
+def delete_favorite_planet(favorite_planet_id):
+
+    item = db.session.execute(db.select(FavoritePlanet).filter_by(id=favorite_planet_id)).first()[0]
+    db.session.delete(item)
+    db.session.commit()
+
+    return jsonify([]), 204
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
